8e19ae293c3b3bc5b1b2beae504d6a2c480da387
aplicativosST <sebastian.torres@enel.com>
2026-03-21 12:51:53 -0500

    third binary test

--- backend/api/index.py
+++ backend/api/index.py
@@ -1,8 +1,1 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/")
-@app.get("/api/")
-def root():
-    return {"message": "index alive"}
+from app__server.main import app
--- backend/app__server/main.py
+++ backend/app__server/main.py
@@ -1,21 +1,8 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
-from app__server.api.routes.rf_routes import router as rf_router
-from app__server.core.config import API_ROOT_PATH
+app = FastAPI()
 
-app = FastAPI(
-    root_path=API_ROOT_PATH,
-    docs_url="/docs",
-    openapi_url="/openapi.json",
-)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-app.include_router(rf_router)
+@app.get("/")
+@app.get("/api/")
+def root():
+    return {"message": "main alive"}
